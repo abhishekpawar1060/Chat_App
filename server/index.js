f020c1ee682e9c2981c471bd64671cc365fa7837
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-10-16 18:32:15 +0530

    Design the Register API

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/connectionDB');
+const router = require("./routes/index");
+
 
 const app = express();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
-}))
+}));
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
+
+app.use('/api', router);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
@@ -19,8 +25,3 @@ connectDB().then(() => {
 })
 
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Server is Running"
-    })
-})
