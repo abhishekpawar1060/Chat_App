3a3974388c3c24941f880a6cce5d3f043f68f93c
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-11-18 16:12:01 +0530

    Add functionality of searching user

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,19 +3,24 @@ import { IoChatboxEllipses } from "react-icons/io5";
 import { FaUserPlus } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
 import { RiLogoutCircleRLine } from "react-icons/ri";
+import { GoArrowUpLeft } from "react-icons/go";
 
 import Avatar from "./Avatar"
 import { useSelector } from 'react-redux';
 import EditUserDetails from './EditUserDetails';
+import SearchUser from './SearchUser';
 
 function Sidebar() {
 
     const user = useSelector(state => state?.user);
 
     const [editUserOpen, setEditUserOpen] = useState(false);
+    const [openSearch, setOpenSearch] = useState(false);
+
+    const [allUser, setAllUser] = useState([]);
 
     return (
-        <div className='w-full h-full'>
+        <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
             <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
                 <div>
                     <NavLink className={({isActive}) => `w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200" }`} title='chat'>
@@ -23,7 +28,8 @@ function Sidebar() {
                             size={20}
                         />
                     </NavLink>
-                    <div className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' title='add user'>
+
+                    <div title='add friend' onClick={() => setOpenSearch(true)} className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
                         <FaUserPlus
                             size={20}
                         />
@@ -47,13 +53,41 @@ function Sidebar() {
                 </div>
             </div>
 
-            {/* Edit user details */}
+            <div className='w-ful'>
+                <div className='h-16 flex items-center'>
+                    <h2 className='text-xl font-bold p-4 text-slate-700'>Message</h2>
+                </div>
+                <div className='bg-slate-200 p-[0.5px]'></div>
 
+                <div className='h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar'>
+                    {
+                        allUser.length === 0 && (
+                            <div className='mt-12'>
+                                <div className='flex justify-center items-center my-4 text-slate-600'>
+                                    <GoArrowUpLeft size={50}/>
+                                </div>
+                                <p className='text-lg text-center text-slate-400'>Explore users to start a conversation with.</p>
+                            </div>
+                        )
+                    }
+                </div>
+
+            </div>
+
+            {/* Edit user details */}
             {
                 editUserOpen && (
                     <EditUserDetails onClose={() => setEditUserOpen(false)} user={user}/>
                 )
             }
+
+            {/* Search User */}
+            {
+                openSearch && (
+                    <SearchUser onClose={() => setOpenSearch(false)}/>
+                )
+            }
+
         </div>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, setUser } from '../redux/userSlice';
 import Sidebar from '../components/Sidebar';
+import logo from "../assets/logo1.png"
 
 const Home = () => {
 
   const user  = useSelector(state => state.user);
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
+  const location = useLocation();
+
   console.log("redux user", user);
   
 
@@ -25,7 +27,7 @@ const Home = () => {
 
       dispatch(setUser(response.data.data));
 
-      if(response.data.logout){
+      if(response.data.data.logout){
         dispatch(logout());
         navigate('/email');
       }
@@ -40,17 +42,31 @@ const Home = () => {
 
   useEffect(() => {
     fetchUserDetails();
-  }, [])
+  }, []);
+
+  
+  const basePath = location.pathname === '/';
 
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-      <section className='bg-white'>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
         <Sidebar />
       </section>
 
-      <section>
+      <section className={`${basePath  && 'hidden'}`}>
         <Outlet />
       </section>
+
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <img 
+            src={logo}
+            width={200}
+            alt='logo'
+          />
+        </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
   )
 }
